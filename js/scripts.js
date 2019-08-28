5c2e361563bda5ece12a16601b869addffef8e4b
Lauren Musante and Neha Malhotra <lauren+neha@>
2019-08-28 15:13:44 -0700

    Change click function to properly add turnScore to totalScore.

--- js/scripts.js
+++ js/scripts.js
@@ -12,29 +12,27 @@ var player2 = new Player();
 $("#roll").click(function(){
   var number = Math.ceil(Math.random() * 6);
   console.log(number);
-  player1.turnScore = number;
+  player1.turnScore += number;
   var result = $("#rollResult").text(number);
   // var result2 = $("#result2").text(number);
-  player1.calculateScore(number);
-  console.log(player1)
+  // player1.calculateScore(number);
+  // console.log(player1)
 });
 
-Player.prototype.calculateTotal = function() {
-  this.totalScore += this.turnScore;
-  return this.totalScore;
 
-}
 
 $("#hold").click(function(){
 player1.calculateTotal();
 console.log(player1);
 });
 
-
+Player.prototype.calculateTotal = function() {
+ this.totalScore = this.turnScore + this.totalScore;
+}
 //
-// var Neha = new Player1 (turnScore, totalScore);
+// var Neha = new Player (turnScore, totalScore);
 // console.log(Neha);
-
+//
 
 Player.prototype.calculateScore = function(randomNumber){
  this.turnScore = randomNumber;
