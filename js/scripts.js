2a9e149c48646d8b730f37824daa3bf0bd3fb241
Lauren Musante and Neha Malhotra <lauren@>
2019-08-28 11:34:53 -0700

    Add function to calculate scores.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,36 @@
 $(document).ready(function(){
+// var turnScore;
+// var totalScore;
+// var result;
+// var result2;
+// var number;
+
+function Player (){
+  this.turnScore = 0;
+  this.totalScore = 0;
+};
+
+var player1 = new Player();
+var player2 = new Player();
 
 $("#roll").click(function(){
   var number = Math.round(Math.random() * 6);
   console.log(number);
-})
+  player1.turnScore = number;
+  var result = $("#result1").text(number);
+  var result2 = $("#result2").text(number);
+  player1.calculateScore(number);
+  console.log(player1)
+});
 
+// 
+// var Neha = new Player1 (turnScore, totalScore);
+// console.log(Neha);
 
+Player.prototype.calculateScore = function(randomNumber){
+ this.turnScore += randomNumber;
+ return this.turnScore ;
 
+};
 
 });
