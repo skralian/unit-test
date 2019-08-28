9f044bd3859bae9e92a436ec9a728b8836deeba6
Lauren Musante and Neha Malhotra <neha@>
2019-08-28 14:17:54 -0700

    add functionality to calculate total function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
-// var turnScore;
-// var totalScore;
-// var result;
-// var result2;
-// var number;
+var number = Math.ceil(Math.random() * 6);
 
 function Player (){
   this.turnScore = 0;
@@ -14,21 +10,34 @@ var player1 = new Player();
 var player2 = new Player();
 
 $("#roll").click(function(){
-  var number = Math.round(Math.random() * 6);
+  var number = Math.ceil(Math.random() * 6);
   console.log(number);
   player1.turnScore = number;
-  var result = $("#result1").text(number);
-  var result2 = $("#result2").text(number);
+  var result = $("#rollResult").text(number);
+  // var result2 = $("#result2").text(number);
   player1.calculateScore(number);
   console.log(player1)
 });
 
-// 
+Player.prototype.calculateTotal = function() {
+  this.totalScore += this.turnScore;
+  return this.totalScore;
+
+}
+
+$("#hold").click(function(){
+player1.calculateTotal();
+console.log(player1);
+});
+
+
+//
 // var Neha = new Player1 (turnScore, totalScore);
 // console.log(Neha);
 
+
 Player.prototype.calculateScore = function(randomNumber){
- this.turnScore += randomNumber;
+ this.turnScore = randomNumber;
  return this.turnScore ;
 
 };
