156849dffb32e194ebddadfee426ead8ad180287
Lauren Musante and Neha Malhotra <lauren@>
2019-08-29 13:41:18 -0700

    fixing conflict

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,48 @@
-$(document).ready(function(){
-var number = Math.ceil(Math.random() * 6);
+var player1 = new Player();
+var player2 = new Player();
+
 
 function Player (){
   this.turnScore = 0;
   this.totalScore = 0;
-  this.player = [player1, player2];
+
 };
 
-var player1 = new Player();
-var player2 = new Player();
-var players = [player1, player2];
+Player.prototype.calculateTotal = function() {
+ this.totalScore = this.turnScore + this.totalScore;
+ this.turnScore = 0;
+}
+
+
+$(document).ready(function(){
+
 
 
 $("#p1roll").click(function(event){
+  console.log("total score:"+ player1.totalScore);
+  console.log("turnScore:" + player1.turnScore);
   event.preventDefault();
   var number = Math.ceil(Math.random() * 6);
-  console.log(number);
+  console.log("number:" + number);
   if(number === 1) {
     player1.turnScore = 0;
     var result = $("#rollResult").text(number);
+  // } if (player1.totalScore >= 100){
+  //   alert ("Player 1 wins!");
   } else {
     player1.turnScore += number;
     var result = $("#rollResult").text(number);
-
   }
 
 });
 
 $("#p1hold").click(function(event){
-  event.preventDefault();
    player1.calculateTotal();
+   console.log("total score:" + player1.totalScore);
     var resultP1 = $("#playerOneResult").text(player1.totalScore);
   });
+
+
 
   $("#p2roll").click(function(){
     var number1 = Math.ceil(Math.random() * 6);
@@ -39,10 +50,13 @@ $("#p1hold").click(function(event){
     // var result = $("#rollResult").text(number1);
     if(number1 === 1) {
       player2.turnScore = 0;
-      var result = $("#rollResult").text(number1);
-    } else {
+      var result1 = $("#rollResult").text(number1);
+    } if (player2.totalScore >= 100){
+      alert ("Player 2 wins!");
+    }
+    else {
       player2.turnScore += number1;
-      var result = $("#rollResult").text(number1);
+      var result1 = $("#rollResult").text(number1);
     };
   });
 
@@ -53,7 +67,4 @@ $("#p1hold").click(function(event){
     });
 
 
-Player.prototype.calculateTotal = function() {
- this.totalScore = this.turnScore + this.totalScore;
-}
 });
