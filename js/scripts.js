6a89d75309285f17b2a0c0faaf856cb2f06b4c05
Lauren Musante and Neha Malhotra <neha@>
2019-08-29 10:28:50 -0700

    Add function to make turnScore equal to 0 when player rolls a 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,29 @@
 $(document).ready(function(){
 var number = Math.ceil(Math.random() * 6);
 
-
 function Player (){
   this.turnScore = 0;
   this.totalScore = 0;
   this.player = [player1, player2];
 };
 
-
 var player1 = new Player();
 var player2 = new Player();
 var players = [player1, player2];
-
 
 
 $("#p1roll").click(function(event){
   event.preventDefault();
   var number = Math.ceil(Math.random() * 6);
   console.log(number);
-  player1.turnScore += number;
-  var result = $("#rollResult").text(number);
+  if(number === 1) {
+    player1.turnScore = 0;
+    var result = $("#rollResult").text(number);
+  } else {
+    player1.turnScore += number;
+    var result = $("#rollResult").text(number);
+
+  }
 
 });
 
@@ -32,8 +35,7 @@ $("#p1hold").click(function(event){
 
   $("#p2roll").click(function(){
     var number1 = Math.ceil(Math.random() * 6);
-    console.log(number);
-    player2.turnScore += number;
+    player2.turnScore += number1;
     var result = $("#rollResult").text(number1);
 
   });
@@ -47,26 +49,8 @@ $("#p1hold").click(function(event){
 
 Player.prototype.calculateTotal = function() {
  this.totalScore = this.turnScore + this.totalScore;
+
 }
 
-
-
-
-
-// Player.prototype.calculateScore = function(randomNumber){
-//  this.turnScore = randomNumber;
-//  return this.turnScore ;
-//
-// };
- // $("#player1").click(function(event){
- //   event.preventDefault();
- //    $("#player2div").hide();
- //    $("#player1div").show();
- // });
- // $("#player2").click(function(event){
- //   event.preventDefault();
- //    $("#player1div").hide();
- //    $("#player2div").show();
- // });
 
 });
