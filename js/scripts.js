4ad922bf9b2e63d7d6c3be52ff31da013869597d
Lauren Musante and Neha Malhotra <lauren@>
2019-08-29 09:45:49 -0700

    add separate roll and hold buttons  for each player

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
 var number = Math.ceil(Math.random() * 6);
-if (this.turnScore === 1) {
 
-}
 
 function Player (){
   this.turnScore = 0;
   this.totalScore = 0;
+  this.player = [player1, player2];
 };
 
 
 var player1 = new Player();
 var player2 = new Player();
 var players = [player1, player2];
-$("#roll").click(function(){
+
+
+
+$("#p1roll").click(function(event){
+  event.preventDefault();
   var number = Math.ceil(Math.random() * 6);
   console.log(number);
   player1.turnScore += number;
@@ -21,24 +24,49 @@ $("#roll").click(function(){
 
 });
 
-
-
-$("#hold").click(function(){
+$("#p1hold").click(function(event){
+  event.preventDefault();
    player1.calculateTotal();
-   this.totalScore = this.turnScore + this.totalScore;
-  var result2 = $("#playerOneResult").text(player1.totalScore);
+    var resultP1 = $("#playerOneResult").text(player1.totalScore);
+  });
 
-console.log(player1);
-});
+  $("#p2roll").click(function(){
+    var number1 = Math.ceil(Math.random() * 6);
+    console.log(number);
+    player2.turnScore += number;
+    var result = $("#rollResult").text(number1);
+
+  });
+
+  $("#p2hold").click(function(event){
+    event.preventDefault();
+     player2.calculateTotal();
+      var resultP1 = $("#playerTwoResult").text(player2.totalScore);
+    });
+
 
 Player.prototype.calculateTotal = function() {
  this.totalScore = this.turnScore + this.totalScore;
 }
 
-Player.prototype.calculateScore = function(randomNumber){
- this.turnScore = randomNumber;
- return this.turnScore ;
 
-};
+
+
+
+// Player.prototype.calculateScore = function(randomNumber){
+//  this.turnScore = randomNumber;
+//  return this.turnScore ;
+//
+// };
+ // $("#player1").click(function(event){
+ //   event.preventDefault();
+ //    $("#player2div").hide();
+ //    $("#player1div").show();
+ // });
+ // $("#player2").click(function(event){
+ //   event.preventDefault();
+ //    $("#player1div").hide();
+ //    $("#player2div").show();
+ // });
 
 });
